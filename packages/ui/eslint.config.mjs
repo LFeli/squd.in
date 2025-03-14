53d86959bd22a3ce145145bc1d25d754622ed4dd
Lucas Araújo <lucasfelipemk@gmail.com>
2025-03-14 18:48:14 -0300

    :wrench: chore: alter @repo name config fog @squd-in

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from "@repo/eslint-config/react-internal";
+import { config } from "@squd-in/eslint-config/react-internal";
 
 /** @type {import("eslint").Linter.Config} */
 export default config;
