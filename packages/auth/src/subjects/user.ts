730a4cb9b0cc9633f2862c782627362898cdf45d
Lucas Araújo <lucasfelipemk@gmail.com>
2025-03-15 14:52:49 -0300

    :recycle: refactor: apply zod and created all subjects of project

--- packages/auth/src/subjects/user.ts
+++ packages/auth/src/subjects/user.ts
@@ -1,1 +1,14 @@
-export type UserSubject = ['create' | 'invite' | 'manage' | 'delete', 'User']
+import { z } from 'zod'
+
+export const userSubject = z.tuple([
+  z.union([
+    z.literal('manage'),
+
+    z.literal('create'),
+    z.literal('invite'),
+    z.literal('delete'),
+  ]),
+  z.literal('User'),
+])
+
+export type UserSubject = z.infer<typeof userSubject>
