444fef9f6d3fbd5994666fd581263cff7f57a02d
Lucas Araújo <lucasfelipemk@gmail.com>
2025-03-15 15:03:41 -0300

    :sparkles: feat: created organization, project and updated user models

---
+++ packages/auth/src/models/organization.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod'
+
+export const organizationSchema = z.object({
+  __typename: z.literal('Organization').default('Organization'),
+
+  id: z.string(),
+  ownerID: z.string(),
+})
+
+export type Organization = z.infer<typeof organizationSchema>
