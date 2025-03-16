915a161185cd1f9d11baeb4b3bd0166d9f7ece3a
Lucas Araújo <lucasfelipemk@gmail.com>
2025-03-16 10:35:26 -0300

    :sparkles: feat: created a get-profile route and registered

---
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -0,0 +1,47 @@
+import { prisma } from '@/http/lib/prisma'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import type { FastifyInstance } from 'fastify/types/instance'
+import z from 'zod'
+
+export async function getProfile(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/profile',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Get authenticated user profile',
+        operationId: 'getProfile',
+
+        response: {
+          200: z.object({
+            user: z.object({
+              id: z.string().uuid(),
+              name: z.string().nullable(),
+              email: z.string().email(),
+              avatarUrl: z.string().url().nullable(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const payload = await request.jwtVerify<{ sub: string }>()
+
+      const user = await prisma.user.findUnique({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          avatarUrl: true,
+        },
+        where: { id: payload.sub },
+      })
+
+      if (!user) {
+        throw new Error('User not found')
+      }
+
+      return reply.status(200).send({ user })
+    }
+  )
+}
--- apps/api/src/http/routes/registered-routes.ts
+++ apps/api/src/http/routes/registered-routes.ts
@@ -2,6 +2,7 @@ import type { FastifyInstance } from 'fastify'
 
 import { authenticateWithPassword } from './auth/authenticate-with-password'
 import { createAccount } from './auth/create-account'
+import { getProfile } from './auth/get-profile'
 
 export async function registeredRoutes(app: FastifyInstance) {
   app.get('/docs/swagger', async (_, reply) => {
@@ -11,4 +12,5 @@ export async function registeredRoutes(app: FastifyInstance) {
   // auth routes
   app.register(createAccount)
   app.register(authenticateWithPassword)
+  app.register(getProfile)
 }
