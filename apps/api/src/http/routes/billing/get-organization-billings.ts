17bdb3d1d7f19c6ec1dc15b326f1b265105f2a3b
Lucas Araújo <lucasfelipemk@gmail.com>
2025-03-18 20:52:44 -0300

    :sparkles: feat: add getOrganizationBilling route for retrieving billing information of an organization

---
+++ apps/api/src/http/routes/billing/get-organization-billings.ts
@@ -0,0 +1,96 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { authMiddleware } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+import { rolesSchema } from '@squd-in/auth'
+import { BadRequestError } from '../_erros/bad-request-error'
+
+export async function getOrganizationBilling(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(authMiddleware)
+    .get(
+      '/organizations/:slug',
+      {
+        schema: {
+          tags: ['Billing'],
+          summary: 'Get billing information for an organization',
+          security: [{ bearerAuth: [] }],
+          operationId: 'getOrganizationBilling',
+
+          params: z.object({
+            slug: z.string(),
+          }),
+
+          response: {
+            200: z.object({
+              billing: z.object({
+                seats: z.object({
+                  amount: z.number(),
+                  unit: z.number(),
+                  price: z.number(),
+                }),
+                projects: z.object({
+                  amount: z.number(),
+                  unit: z.number(),
+                  price: z.number(),
+                }),
+                total: z.number(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+
+        const userID = await request.getCurrentUserId()
+        const { membership, organization } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userID, membership.role)
+
+        if (cannot('get', 'Billing')) {
+          throw new BadRequestError(
+            "Ypu're not allowed to get billing details from this organization"
+          )
+        }
+
+        const [amountOfMembers, amountOfProjects] = await Promise.all([
+          prisma.member.count({
+            where: {
+              organizationId: organization.id,
+              role: { not: 'BILLING' },
+            },
+          }),
+
+          prisma.project.count({
+            where: {
+              organizationId: organization.id,
+            },
+          }),
+        ])
+
+        const billing = {
+          seats: {
+            amount: amountOfMembers,
+            unit: 10,
+            price: amountOfMembers * 10,
+          },
+          projects: {
+            amount: amountOfProjects,
+            unit: 20,
+            price: amountOfProjects * 20,
+          },
+          total: amountOfMembers * 10 + amountOfProjects * 20,
+        }
+
+        return reply.status(200).send({
+          billing,
+        })
+      }
+    )
+}
--- apps/api/src/http/routes/registered-routes.ts
+++ apps/api/src/http/routes/registered-routes.ts
@@ -6,6 +6,7 @@ import { createAccount } from './auth/create-account'
 import { getProfile } from './auth/get-profile'
 import { requestPasswordRecover } from './auth/request-password-recover'
 import { resetPassword } from './auth/reset-password'
+import { getOrganizationBilling } from './billing/get-organization-billings'
 import { acceptInvite } from './invites/accept-invite'
 import { createInvite } from './invites/create-invite'
 import { getInvitesDetails } from './invites/get-invite-details'
@@ -71,4 +72,7 @@ export async function registeredRoutes(app: FastifyInstance) {
   app.register(acceptInvite)
   app.register(rejectInvite)
   app.register(revokeInvite)
+
+  // billing routes
+  app.register(getOrganizationBilling)
 }
