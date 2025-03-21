ff4749b8812e514f20d7f4a9c60a7e55015f682e
Lucas Araújo <lucasfelipemk@gmail.com>
2025-03-20 21:06:26 -0300

    feat: add orval configuration for API generation

---
+++ apps/web/orval.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'orval'
+
+export default defineConfig({
+  api: {
+    input: 'http://localhost:3333/docs/json',
+
+    output: {
+      target: './src/http/api.ts',
+
+      baseUrl: 'http://localhost:3333',
+      client: 'fetch',
+      httpClient: 'fetch',
+      clean: true,
+
+      override: {
+        fetch: {
+          includeHttpResponseReturnType: false,
+        },
+      },
+    },
+  },
+})
