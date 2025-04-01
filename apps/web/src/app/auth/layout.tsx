7c545352164cb4647dcb77c5d86e0ae633d67775
Lucas Araújo <lucasfelipemk@gmail.com>
2025-03-31 23:15:37 -0300

    :sparkles: feat: add authentication check in AuthLayout to redirect authenticated users

--- apps/web/src/app/auth/layout.tsx
+++ apps/web/src/app/auth/layout.tsx
@@ -1,10 +1,17 @@
-import { Toaster } from '@/components/ui/sonner'
+import { redirect } from 'next/navigation'
 
-export default function AuthLayout({
+import { Toaster } from '@/components/ui/sonner'
+import { isAuthenticated } from '@/helpers/auth'
+
+export default async function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  if (await isAuthenticated()) {
+    redirect('/')
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-4">
       <div className="w-full max-w-xs">{children}</div>
