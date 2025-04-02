11c2c99f636fcd2ab07d96957fae1dca1cf46b2a
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-01 23:24:59 -0300

    :sparkles: feat: add ProfileButton component for user authentication and dropdown menu

---
+++ apps/web/src/components/layout/header/profile-button.tsx
@@ -0,0 +1,45 @@
+import { ChevronsUpDownIcon, LogOutIcon } from 'lucide-react'
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+import { authenticateUser } from '@/helpers/auth'
+import { getInitials } from '@/utils/get-initials'
+
+export async function ProfileButton() {
+  const { user } = await authenticateUser()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex items-center gap-4 outline-none">
+        <Avatar>
+          {user.avatarUrl && <AvatarImage src={user.avatarUrl} />}
+          {user.name && (
+            <AvatarFallback>{getInitials(user.name)}</AvatarFallback>
+          )}
+        </Avatar>
+
+        <div className="flex flex-col items-start">
+          <span className="font-medium text-sm">{user.name}</span>
+          <span className="text-muted-foreground text-xs">{user.email}</span>
+        </div>
+
+        <ChevronsUpDownIcon className="size-4 text-muted-foreground" />
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end" sideOffset={8} className="min-w-56">
+        <DropdownMenuItem asChild>
+          <a href="/api/auth/sign-out">
+            <LogOutIcon className="mr-2 size-4" />
+            Sign Out
+          </a>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
