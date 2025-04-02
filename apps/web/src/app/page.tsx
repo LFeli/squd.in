73090037f408a1e8b3d519d871571acd6058c22c
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-01 23:23:47 -0300

    :sparkles: feat: enable dark mode in RootLayout and remove unused Home page component

--- apps/web/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { authenticateUser } from '@/helpers/auth'
-
-export default async function Home() {
-  const { user } = await authenticateUser()
-
-  return <pre>{JSON.stringify(user, null, 2)}</pre>
-}
