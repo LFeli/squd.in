95aeefac314d31c90d65c8433f25dd769889ebe0
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-02 22:01:04 -0300

    :sparkles: feat: create ProjectsPage component with HeaderComponent integration

---
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -0,0 +1,10 @@
+import { HeaderComponent } from '@/components/layout/header/header'
+
+export default async function ProjectsPage() {
+  return (
+    <div className="py-4">
+      <HeaderComponent />
+      <main>a</main>
+    </div>
+  )
+}
