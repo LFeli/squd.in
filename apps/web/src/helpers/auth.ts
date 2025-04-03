193dc2c5e0ed5fbdf6d5a47cc8cb22ca033a4651
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-03 20:53:22 -0300

    :sparkles: feat: add functions to manage organization membership and user abilities

--- apps/web/src/helpers/auth.ts
+++ apps/web/src/helpers/auth.ts
@@ -1,13 +1,58 @@
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
-import { getProfile } from '@/http/api'
+import { getMembership, getProfile } from '@/http/api'
 import { isHttpError } from '@/utils/is-http-error'
+import { defineAbilityFor } from '@squd-in/auth'
 
 export async function isAuthenticated() {
   const cookie = await cookies()
 
   return Boolean(cookie.get('token')?.value)
+}
+
+export async function getCurrentOrg() {
+  const cookie = await cookies()
+  const org = cookie.get('org')?.value
+
+  return org ?? null
+}
+
+export async function getCurrentMembership() {
+  const org = await getCurrentOrg()
+  const token = await isAuthenticated()
+
+  if (!org) {
+    return null
+  }
+
+  const {
+    data: { membership },
+    status,
+  } = await getMembership(org, {
+    headers: { Authorization: `Bearer ${token}` },
+  })
+
+  if (isHttpError(status)) {
+    return null
+  }
+
+  return membership
+}
+
+export async function ability() {
+  const membership = await getCurrentMembership()
+
+  if (!membership) {
+    return null
+  }
+
+  const ability = defineAbilityFor({
+    id: membership.userId,
+    role: membership.role,
+  })
+
+  return ability
 }
 
 export async function authenticateUser() {
