3f96c7c3166cf7e8ae25d954440dc588e1ec7dd2
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-07 22:30:42 -0300

    :sparkles: feat: create organization sheet component and remove obsolete test routes

---
+++ apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
@@ -0,0 +1,23 @@
+import { OrganizationForm } from '@/components/org/organization-form'
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from '@/components/ui/sheet'
+
+export default function CreateOrganizationSheet() {
+  return (
+    <Sheet defaultOpen open={true}>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Create organization</SheetTitle>
+        </SheetHeader>
+
+        <div className="p-4">
+          <OrganizationForm />
+        </div>
+      </SheetContent>
+    </Sheet>
+  )
+}
