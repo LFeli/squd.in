901f0deb6323c84834539bcab9908e06c7e6fcd6
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-09 22:25:05 -0300

    :sparkles: feat: refactor CreateOrganizationPage to redirect to home on load if can access directly

--- apps/web/src/app/(app)/create-organization/page.tsx
+++ apps/web/src/app/(app)/create-organization/page.tsx
@@ -1,16 +1,5 @@
-import { HeaderComponent } from '@/components/layout/header/header'
-import { OrganizationForm } from '@/components/org/organization-form'
+import { redirect } from 'next/navigation'
 
-export default function CreateOrganizationPage() {
-  return (
-    <div className="space-y-4 py-4">
-      <HeaderComponent />
-
-      <main className="mx-auto w-full max-w-[1200px] space-y-4">
-        <h1 className="font-bold text-2xl">Create organization</h1>
-
-        <OrganizationForm />
-      </main>
-    </div>
-  )
+export default async function CreateOrganization() {
+  redirect('/')
 }
