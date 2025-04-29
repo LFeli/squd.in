6fae93372bf1ae6287ffdfedbac83f44c49d02c0
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-28 23:06:45 -0300

    :sparkles: feat: update orval configuration to use environment variable for API URL

--- apps/web/orval.config.ts
+++ apps/web/orval.config.ts
@@ -1,12 +1,17 @@
+import { env } from '@squd-in/env'
 import { defineConfig } from 'orval'
+
+const API_URL = env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3333'
+
+const API_INPUT = `${API_URL}/docs/json`
 
 export default defineConfig({
   api: {
-    input: 'http://localhost:3333/docs/json',
+    input: API_INPUT,
 
     output: {
       mode: 'single',
-      baseUrl: 'http://localhost:3333',
+      baseUrl: API_URL,
 
       target: './src/http/api.ts',
       httpClient: 'fetch',
