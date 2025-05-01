3e772f48ae8d3316a5d25576f93e1dfcdc025294
Lucas Araújo <lucasfelipemk@gmail.com>
2025-05-01 19:49:13 -0300

    feat: implement createProjectAction for handling project creation logic

---
+++ apps/web/src/app/(app)/org/[slug]/project/create-project/actions.ts
@@ -0,0 +1,38 @@
+// import { createProject } from '@/http/api'
+import type { FormState } from '@/types/form'
+
+import { createProjectFormSchema } from './validation'
+
+export async function createProjectAction(data: FormData): Promise<FormState> {
+  const result = createProjectFormSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    return {
+      success: false,
+      message: 'Something went wrong, please try again',
+      errors: null,
+    }
+  }
+
+  const { name, description } = result.data
+
+  try {
+    // await createProject(shouldAttachUsersByDomain, {
+    //   name,
+    //   description,
+    // })
+    console.log('i stay in try...')
+  } catch {
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+
+  return {
+    success: true,
+    message: 'Successfully saved the project.',
+    errors: null,
+  }
+}
