d3f49ddc7f81f21503fee358a24cf0dfd6200ccf
Lucas Araújo <lucasfelipemk@gmail.com>
2025-05-03 11:37:20 -0300

    docs: add JSDoc comments to clarify the purpose of the CreateOrganization route and its redirect behavior

--- apps/web/src/app/(app)/create-organization/page.tsx
+++ apps/web/src/app/(app)/create-organization/page.tsx
@@ -1,5 +1,16 @@
 import { redirect } from 'next/navigation'
 
-export default async function CreateOrganization() {
+/**
+ * This route is used solely to support intercepted routing in Next.js.
+ *
+ * When accessed, it immediately redirects the user to the create organization page.
+ * The intercepted route mechanism relies on having an actual route file,
+ * even if it's just a redirect.
+ *
+ * Redirect target: /create-organization
+ *
+ * @see https://nextjs.org/docs/app/building-your-application/routing/intercepting-routes
+ */
+export default async function CreateOrganizationRoute() {
   redirect('/')
 }
